89296d2c82abe1c9ff313b8565517761b015fab9
dreaMykj <gabrielgreghi2018@gmail.com>
2023-11-01 12:34:10 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Números pares entre 0 e ?: ");
+int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+if(numeroDigitado >=1 )
+{
+    int numeroPar = 0;
+
+    while (numeroPar <= numeroDigitado)
+    {
+        Console.Write($"{numeroPar} ");
+        numeroPar += 2;
+    }
+}
+else
+{
+    Console.WriteLine($"O número {numeroDigitado} não é inteiro positivo.");
+}
